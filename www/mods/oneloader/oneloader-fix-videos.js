4405ae4687b6de79131b5152db1ac7e6f5f9d13f
Rph <rphsoftware@gmail.com>
2021-07-11 18:20:34 +0200

    More robust testing before revoking object URL for videos

--- www/mods/oneloader/oneloader-fix-videos.js
+++ www/mods/oneloader/oneloader-fix-videos.js
@@ -7,8 +7,8 @@ PIXI.VideoBaseTexture.fromUrl = function fromUrl(videoSrc, scaleMode, crossorigi
         let objectURL = URL.createObjectURL(b);
         let vidObj = old.call(this, {src: objectURL, mime: "video/" + videoSrc.split(".")[videoSrc.split(".").length - 1]}, scaleMode, crossorigin, autoPlay);
         let a = setInterval(function( ){
-            if (vidObj.source.buffered.end(0) >= vidObj.source.duration) {
-                console.log("Revoking url object url");
+            if (vidObj.source.readyState === 4 && (vidObj.source.buffered.end(0) >= vidObj.source.duration) && vidObj.source.ended) {
+                window._logLine("Revoking object url");
                 URL.revokeObjectURL(objectURL);
                 clearInterval(a);
             }
