af3d0a814158db8862c7ffbeebd3d63b6f6bc2fa
Rph <rphsoftware@gmail.com>
2021-07-17 04:56:55 +0200

    Fix improved save & load breakage on nw 0.54.1

--- www/modloader/vfs_node.js
+++ www/modloader/vfs_node.js
@@ -326,6 +326,8 @@ async function _modLoader_install_node_vfs(shadowfs, nativefs) {
         async rename(o, n, callback) {
             let [mode_1] = determine_location(o);
             let [mode_2] = determine_location(n);
+            if (!callback) callback = () => {}; // thanks improved save & load for being incompliant with node.js apis
+
             if (mode_1 === 0 && mode_2 === 0) return nativefs.rename(...arguments);
             else {
                 window._logLine("Something tried to rename to or from a virtual/overlayed directory. The write was ignored. Path: " + file);
