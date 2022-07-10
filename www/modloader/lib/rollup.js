4728f57e347fa4a813e4b6558b1f96e699efa2ae
Rph <rphsoftware@users.noreply.github.com>
2022-07-11 01:09:22 +0200

    Fix path relativity in rollup loader. Fixes #13

--- www/modloader/lib/rollup.js
+++ www/modloader/lib/rollup.js
@@ -1,10 +1,12 @@
 // This sucks hoping we move to ES Modules soon!
 // For now we shouldn't pollute the global with things defined as const
 {
+  const path = require('path');
+  const base = path.dirname(process.mainModule.filename);
   const fs = require('fs');
 
   window._loadRollup = async function () {
-    const data = fs.readFileSync('./modloader/lib/rollup.browser.js');
+    const data = fs.readFileSync(path.join(base, 'modloader/lib/rollup.browser.js'));
 
     let scriptBlob = new Blob([data.toString("utf-8")], { type: "application/javascript" });
     let url = URL.createObjectURL(scriptBlob);
