50d2da1aadc429a7d91d3cbea877ec6ca5937f0a
Rph <rphsoftware@users.noreply.github.com>
2022-07-11 01:08:09 +0200

    Remove the definition of `fs` as a global

--- www/modloader/lib/rollup.js
+++ www/modloader/lib/rollup.js
@@ -1,14 +1,17 @@
 // This sucks hoping we move to ES Modules soon!
-const fs = require('fs');
+// For now we shouldn't pollute the global with things defined as const
+{
+  const fs = require('fs');
 
-window._loadRollup = async function () {
-  const data = fs.readFileSync('./modloader/lib/rollup.browser.js');
+  window._loadRollup = async function () {
+    const data = fs.readFileSync('./modloader/lib/rollup.browser.js');
 
-  let scriptBlob = new Blob([data.toString("utf-8")], { type: "application/javascript" });
-  let url = URL.createObjectURL(scriptBlob);
+    let scriptBlob = new Blob([data.toString("utf-8")], { type: "application/javascript" });
+    let url = URL.createObjectURL(scriptBlob);
 
-  const { rollup } = await import(url);
-  URL.revokeObjectURL(url);
+    const { rollup } = await import(url);
+    URL.revokeObjectURL(url);
 
-  return rollup;
+    return rollup;
+  }
 }
