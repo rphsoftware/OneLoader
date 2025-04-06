9611fd9860241390ca425db1f8f3cb7335c14649
Rph :3 <11350302+rphsoftware@users.noreply.github.com>
2025-04-06 22:48:11 +0200

    Catgirl!!!! Credits: bajamaid for the catgirl pixelart <3 @kodicraft for helping with the jump animation

--- www/modloader/oneloader-loading-gui2.js
+++ www/modloader/oneloader-loading-gui2.js
@@ -1,3 +1,6 @@
+// Image credit: Bajamaid
+const hot_catgirl = `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAZlBMVEX///8yNz5AQlGVqMEAAAD1+f/rv66/cV4cHSSetM/99+52WUcqLD7B0uCut8KAk8f/3NZwcHD++/X029LM4PXNwdY+VXCSs9x9nMKszOxqdd0hLFQdITCZsP/q+P/D1N1vfpI8SXYiyUzWAAAAAXRSTlMAQObYZgAAANtJREFUeNp0jIUWBCEIAA10e6+78P9/8rAuYTaMmYf6ILxXlnALdRWCazTkr2KACTnARwJRCIqPieDvxd/5AslkHlLwRg5QCqJHAPAY/w8MgQlg3qzAw/54gnC7BS5YrQFgKwUW5uUSetgdD3CJ/i+wAND3Pf0vZzYgmoi1XIDR6nGcxtFiODMDmsZ7bZxzxqL/913X9T0FxhkKmn/vXOeHIQda/dNN0zBYq7XThOKKhSVv4oQ4ggOrF4voK1r2ciEG70K/NedjUlESjffxVSKtMfFVMm37HAiRBgAiGRaKlPJ1XQAAAABJRU5ErkJggg==`;
+
 window.$oneLoaderGui = new (class OneLoaderGui {
     constructor() {
         this.easterEgg = {
@@ -8,6 +11,16 @@ window.$oneLoaderGui = new (class OneLoaderGui {
         this.container = document.createElement("div");
         this.container.style = "font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; background: #222; color: white; width: 100vw; height: 100vh; position: relative; display: flex; flex-direction: column; align-items: center; justify-content: center;";
         requestAnimationFrame(() => { this.tryMount(); });
+
+        let catgirlScale = Math.floor(Math.floor(Math.min(window.innerWidth, window.innerHeight) / 3) / 32);
+
+        let catgirlAnimationOptions = [
+            ["__oneloader_catgirl__1_", "2s linear"],
+            ["__oneloader_catgirl__2_", "2s cubic-bezier(0.68, 1.1, 0.59, 0.79)"],
+            ["__oneloader_catgirl__3_", "1s ease"]
+        ];
+
+        let pickedOption = catgirlAnimationOptions[Math.floor(Math.random() * catgirlAnimationOptions.length)];
 
         this.container.innerHTML = `
         <div class="header-container" style="display: flex; justify-content: center; position: fixed; top: 0; left: 0; right: 0; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif">
@@ -46,6 +59,43 @@ window.$oneLoaderGui = new (class OneLoaderGui {
     <div style="position: fixed; bottom: 0; left: 0; color: #666; ">
         v<span data-ol="vernum">???</span>
     </div>
+    <style>
+        #__oneloader_catgirl__ {
+            animation: ${pickedOption[0]} ${pickedOption[1]} infinite;
+            transform-origin: center;
+        }
+        @keyframes __oneloader_catgirl__1_ {
+            0% {
+                transform: rotateY(0deg);
+            }
+            100% {
+                transform: rotateY(360deg);
+            }
+        }
+        @keyframes __oneloader_catgirl__2_ {
+            0% {
+                transform: scale(1);
+            }
+            50% {
+                transform: scale(0);
+            }
+            100% {
+                transform: scale(1);
+            }
+        }
+        @keyframes __oneloader_catgirl__3_ { /* Credit: @kodicraft */
+            0% {
+                transform: translate(0px, 0px) scale(1.0, 1.0);
+            }
+            25% {
+                transform: translate(0px, -128px) scale(1.0, 1.0);
+            }
+            50% {
+                transform: translate(0px, 0px) scale(1.0, 1.0);
+            }
+        }
+    </style>
+    <img id="__oneloader_catgirl__" width="${catgirlScale * 32}" height="${catgirlScale * 32}" src="${hot_catgirl}" style="position: fixed; bottom: 0; right: 0; image-rendering: pixelated; z-index: 0;">
         `;
         this.vernum = this.container.querySelector("[data-ol=vernum]");
         this.percentage = this.container.querySelector("[data-ol=percentage]");
@@ -58,6 +108,7 @@ window.$oneLoaderGui = new (class OneLoaderGui {
         if ((new Date()).getDate() === this.easterEgg.day && (new Date()).getMonth() === this.easterEgg.month) {
             v = v + " || " + this.easterEgg.text;
         }
+        v = v + " || OneLoader catgirl by bajamaid"
         this.vernum.innerText = v;
     }
 
